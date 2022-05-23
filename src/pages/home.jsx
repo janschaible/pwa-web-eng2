28760df81bafe0d6b2ff96df738ed2392bf59cc9
muellermili <janphilip.schaible@gmail.com>
2022-05-23 20:44:06 +0200

    add map

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Page,
+  Navbar,
+  NavTitle,
+  NavTitleLarge
+} from 'framework7-react';
+import MapComponent from '/components/Map/Map'
+
+const HomePage = () => (
+  <Page name="home">
+    {/* Top Navbar */}
+    <Navbar large>
+      <NavTitle>pwa-web-eng2</NavTitle>
+      <NavTitleLarge>pwa-web-eng2</NavTitleLarge>
+    </Navbar>
+    <MapComponent/>
+  </Page>
+);
+export default HomePage;
