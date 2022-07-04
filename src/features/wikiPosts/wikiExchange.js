0c0f4354460d9aa1d56534ffa1a1116d55ee031e
irko <98326099+Irko42@users.noreply.github.com>
2022-07-04 12:45:05 +0200

    deleted unnecessary file

--- src/features/wikiPosts/wikiExchange.js
+++
@@ -1,7 +0,0 @@
-import { setCurrentPosition } from "../routing/routingSlice";
-
-export const coords = () => {
-    const ladertyp = setCurrentPosition.checkLat;
-    const londertyp = setCurrentPosition.checkLon;
-    return ladertyp,londertyp;
-}
